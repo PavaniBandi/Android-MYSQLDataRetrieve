f3d2f7ae0f5c617742923cd21c269f9e4ba6d66b
PavaniBandi <pavanishiny@gmail.com>
2016-11-11 00:26:34 +0530

    Signed-off-by: PavaniBandi <pavanishiny@gmail.com>

--- app/src/main/java/com/pavani/mysqldataretrieve/MainActivity.java
+++ app/src/main/java/com/pavani/mysqldataretrieve/MainActivity.java
@@ -1,15 +1,11 @@
 package com.pavani.mysqldataretrieve;
 
-
+import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.*;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -17,10 +13,11 @@ import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends Activity implements View.OnClickListener {
     private Button b;
     private TextView tv;
     @Override
