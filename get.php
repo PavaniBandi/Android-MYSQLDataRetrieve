2fb1747993b77a51ebe4b28234f70acb61fcf024
PavaniBandi <PavaniBandi@users.noreply.github.com>
2016-11-11 00:30:02 +0530

    Add files via upload

---
+++ get.php
@@ -0,0 +1,37 @@
+<?php
+
+
+$host='localhost';
+    $user='root';
+    $password='';
+    
+    
+	
+	$connection = mysqli_connect($host,$user,$password);
+	
+	if(!$connection){
+		die('Connection Failed');
+	}
+	else{
+		$dbconnect = mysqli_select_db($connection,'details');
+		
+		if(!$dbconnect){
+			die('Could not connect to Database');
+		}
+		else{
+            
+            $query = "SELECT * from student ";
+            
+            if($resultset = mysqli_query( $connection,$query))
+            {
+			$records= array();
+              
+			while($r = mysqli_fetch_assoc($resultset)){
+				$records[] = $r;
+			}
+                
+                echo json_encode($records);
+		}
+	}
+}
+?>
