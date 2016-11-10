c52566da512a028d265ddbd1c816c85efd8b5308
PavaniBandi <pavanishiny@gmail.com>
2016-11-10 23:59:44 +0530

    Signed-off-by: PavaniBandi <pavanishiny@gmail.com>

--- app/src/main/java/com/pavani/mysqldataretrieve/MainActivity.java
+++ app/src/main/java/com/pavani/mysqldataretrieve/MainActivity.java
@@ -1,13 +1,89 @@
 package com.pavani.mysqldataretrieve;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button b;
+    private TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b=(Button)findViewById(R.id.button);
+        tv=(TextView)findViewById(R.id.textView);
+
+        b.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        new receive().execute();
+    }
+
+    class receive extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            try {
+                URL url = new URL("http://192.168.0.105/get.php");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                } finally {
+                    urlConnection.disconnect();
+                }
+            } catch (Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+
+        }
+
+        protected void onPostExecute(String response) {
+            String Name;
+            if (response == null) {
+                Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
+            }
+            else {
+                try {
+                    JSONArray jArray = new JSONArray(response);
+                    tv.setText("Names:");
+                    for (int p = 0; p < jArray.length(); p++) {
+                        JSONObject json_data = jArray.getJSONObject(p);
+                        Name = json_data.getString("name");
+                        tv.setText(tv.getText()+"\n"+Name);
+                        }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
+
